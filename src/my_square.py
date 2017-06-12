710582b92996ccfd26be7cb1568b62c1983a0441
William Han <whan@godaddy.com>
2017-06-11 23:08:17 -0700

    add and update documentation for squares

--- src/my_square.py
+++ src/my_square.py
@@ -1,8 +1,11 @@
 def my_square(y):
-  """uses the ** operator"""
+  """uses the ** operator
+  """
   return(y ** 2)
 
 def my_square2(x):
+  """uses the * operator to calculate square
+  """
   return(x * x)
 
 print(my_square(42))
